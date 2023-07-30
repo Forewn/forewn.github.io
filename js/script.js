92f48f77ba3da5a85580bb64f3cd6d97c6c7d572
Jhosmar Suarez <jhosmardsuarezc961@gmail.com>
2023-07-30 07:17:22 -0400

    b

--- js/script.js
+++ js/script.js
@@ -2,10 +2,6 @@ const menu = document.querySelector('.navmobile');
 const options = document.querySelector('navmobile-options');
 const loader = document.querySelector('.custom-loader');
 
-menu.addEventListener('click', () =>{
-    link.setAttribute('src', 'css/change.css');
-});
-
 window.addEventListener("load", () => {
     loader.setAttribute('style', 'display:none');
     console.log("cargue");

db87ed4397258ebc6b341cd9b97ea998a5a65991
Jhosmar Suarez <jhosmardsuarezc961@gmail.com>
2023-07-30 08:03:30 -0400

    working?

--- js/script.js
+++ js/script.js
@@ -1,9 +1,26 @@
-window.addEventListener("DOMContentLoaded", () => {
-    const loader = document.querySelector('.custom-loader');
-  
-    loader.setAttribute('style', 'display:block');
+const loader = document.querySelector('.custom-loader');
+const header = document.querySelector('.mainbar');
+const footer = document.querySelector('footer');
+const content = document.querySelector('main');
+const menubtn = document.querySelector('.navmobile');
+const options = document.querySelector('.navmobile-options');
 
+
+
+window.addEventListener('DOMContentLoaded', () => {
+    loader.setAttribute('style', 'opacity:1;');
+    header.setAttribute('style', 'opacity:1;');
+    footer.setAttribute('style', 'opacity:0');
+    content.setAttribute('style', 'opacity:0;');
     window.addEventListener('load', ()=>{
-        loader.setAttribute('style', 'display:none;');
-      })
-  });
+        loader.setAttribute('style', 'opacity:0;');
+        header.setAttribute('style', 'opacity:1;');
+        footer.setAttribute('style', 'opacity:1');
+        content.setAttribute('style', 'opacity:1;');
+    });
+    
+});
+
+menubtn.addEventListener('click', ()=> {
+    options.setAttribute('style', 'display:block;');
+});

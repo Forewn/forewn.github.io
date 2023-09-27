9a275ebc5aeed8ec746e7de9b676e203449cb347
Jhosmar Suarez <jhosmardsuarezc961@gmail.com>
2023-09-27 12:46:00 -0400

    si

--- js/login_validation.js
+++ js/login_validation.js
@@ -2,6 +2,7 @@
 // de momento solo alerta que no hay conexion con la db
 
 const button = document.querySelector('button');
+const agree = document.querySelector('')
 
 button.addEventListener('click', () =>{
     alert('Base de datos desconectada');
